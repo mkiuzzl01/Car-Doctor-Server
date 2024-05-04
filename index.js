4474b3d6e7053927cb34e350ee5ac5de55851e8b
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-05-04 23:58:12 +0600

    spacific services api is created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,15 +24,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('Car-Doctor').collection('Services');
+
+    app.get('/Services', async (req,res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/Services/:id', async (req,res)=>{
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+        const options = {
+            projection:{title:1,price:1},
+        }
+       const result = await serviceCollection.findOne(query,options)
+       res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
