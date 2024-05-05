cacd9d7d8054d71411c11eb8688670b84416e5e3
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-05-05 20:38:09 +0600

    car-doctor server some updated

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db('Car-Doctor').collection('Services');
+    const serviceCollection = client.db('Car-Doctor').collection('Services'); 
+    const bookingCollection = client.db('Car-Doctor').collection('booking');
 
     app.get('/Services', async (req,res)=>{
         const cursor = serviceCollection.find();
@@ -39,10 +40,46 @@ async function run() {
        const id = req.params.id;
        const query = {_id: new ObjectId(id)};
         const options = {
-            projection:{title:1,price:1},
+            projection:{title:1,price:1,img :1},
         }
        const result = await serviceCollection.findOne(query,options)
        res.send(result);
+    })
+
+    app.get("/Booking", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = {customerEmail:req.query.email}
+      }
+      console.log(query);
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+    
+    app.post('/Booking', async(req,res)=>{
+      const bookingInfo = req.body;
+      const result = await bookingCollection.insertOne(bookingInfo);
+      res.send(result); 
+
+    })
+    app.patch('/Booking/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBooking = req.body;
+      const updateDoc = {
+        $set:{
+          status:updatedBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter,updateDoc);
+      res.send(result);
+
+    })
+    app.delete('/Booking/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
